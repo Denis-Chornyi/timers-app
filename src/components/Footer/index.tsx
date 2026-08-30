9ae98ec7e038bbdbef6842473847784f9e8a8b5c
Denis Chornyi <chornyi.den@gmail.com>
2026-08-31 00:06:24 +0300

    fix

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,7 +1,7 @@
 const Footer: React.FC = () => {
   return (
     <section className="bg-[#fff] shadow-[0px_-1px_1px_-1px_rgba(0,_0,_0,_0.4)]">
-      <div className="m-[0_auto] flex min-h-[110px] w-[1440px] items-center px-[16px] xl:px-[135px]">
+      <div className="m-[0_auto] flex min-h-[110px] items-center px-[16px] xl:px-[135px]">
         <div className="m-[0_auto] flex w-[1440px] items-center justify-between">
           <h1 className="text-[20px] font-[800]">Timers App</h1>
           <div className="ml-[12px] hidden text-center text-[11px] tracking-[0.17px] text-[#676c75] md:block">
